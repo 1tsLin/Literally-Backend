5a7157b1ea8397e7f6118418f81ec34fb858dc4c
Lin <julinejiang@gmail.com>
2026-03-10 15:00:30 +0100

    init spring boot project

---
+++ src/test/java/com/literally/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literally.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
